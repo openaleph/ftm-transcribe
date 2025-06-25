4c00a188f562c2a2720b90f7b395608463efa3f5
Alex Ștefănescu <alex.stefanescu@pm.me>
2025-06-25 15:04:13 +0200

    Implement the transcription as a procrastinate task

--- ftm_transcribe/settings.py
+++ ftm_transcribe/settings.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,11 +9,27 @@ class Settings(BaseSettings):
 
     Note:
         All settings can be set via environment variables in uppercase,
-        prepending `FTMTR_` (except for those with a given prefix)
-
+        prepending `FTMTR_`
     """
 
-    model_config = SettingsConfigDict(env_prefix="ftmtr_")
+    model_config = SettingsConfigDict(
+        env_prefix="ftmtr_", env_file=".env", extra="ignore"
+    )
 
-    # language
-    # model
+    data_root: str = Field()
+    """Root directory; used for temporarily writing output files"""
+
+    whisper_executable: str = Field()
+    """Path to whisper-cli"""
+
+    whisper_model_root: str = Field()
+    """Path to WhisperCpp model, usually ./models"""
+
+    whisper_model: str = Field(default="ggml-medium-q8_0.bin")
+    """Name of WhisperCpp model, inside the model_root directory"""
+
+    whisper_timeout: int = Field(default=60 * 60)
+    """Timeout for the transcription operation and the ffmpeg audio extraction"""
+
+    whisper_language: str = Field(default="auto")
+    """Two-letter language code (ISO 639-1) or auto"""
--- ftm_transcribe/tasks.py
+++ ftm_transcribe/tasks.py
@@ -1,22 +1,145 @@
+import json
+import logging
+import subprocess
+from pathlib import Path
+
 from openaleph_procrastinate.app import make_app
-from openaleph_procrastinate.model import DatasetJob
+from openaleph_procrastinate.model import DatasetJob, Defers
 from openaleph_procrastinate.tasks import task
+from openaleph_procrastinate import defer
+from followthemoney.proxy import EntityProxy
 
 from ftm_transcribe.settings import Settings
+from ftm_transcribe.exceptions import ProcessingException
 
 settings = Settings()
+
+log = logging.getLogger(__name__)
 app = make_app(__loader__.name)
 
 ORIGIN = "ftm-transcribe"
 
 
 @task(app=app)
-def transcribe(job: DatasetJob) -> DatasetJob:
-    print("AAAAAA")
-    # for entity_file_reference in job.get_file_references():
-    #     local_path = entity_file_reference.get_localpath()
-    #     print(local_path)
-    # for entity, handler in job.get_file_references():
-    #     with handler.get_localpath() as path:
-    #         subprocess.run(f"some-program -f {path}")
-    return job
+def transcribe(job: DatasetJob) -> Defers:
+    for entity_file_reference in job.get_file_references():
+        entity: EntityProxy = entity_file_reference.entity
+        audio_only_path = None
+
+        with entity_file_reference.get_localpath() as local_path:
+            try:
+                audio_only_path = get_audio_only_path(local_path)
+                full_transcription = get_transcription_text(
+                    audio_only_path, entity_file_reference.entity
+                )
+
+                entity.add("bodyText", full_transcription)
+
+                with job.get_writer() as bulk:
+                    bulk.put(entity)
+
+                yield defer.analyze(job.dataset, [entity], **job.context)
+            except ProcessingException as e:
+                log.error(f"Transcription failed: {e}")
+            finally:
+                if audio_only_path:
+                    _delete_temporary_file(audio_only_path)
+
+
+def get_audio_only_path(file_path: Path) -> Path:
+    audio_only_path = Path(settings.data_root) / file_path.parts[-1].split(".")[0]
+    audio_only_path = audio_only_path.with_suffix(".wav")
+
+    # https://github.com/ggml-org/whisper.cpp?tab=readme-ov-file#quick-start
+    cmd = [
+        "ffmpeg",
+        "-y",
+        "-i",
+        file_path,
+        "-ar",
+        "16000",
+        "-ac",
+        "1",
+        "-c:a",
+        "pcm_s16le",
+        audio_only_path,
+    ]
+
+    try:
+        subprocess.run(
+            cmd,
+            timeout=int(settings.whisper_timeout),
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError as e:
+        raise ProcessingException(e)
+
+    if not audio_only_path.is_file():
+        raise ProcessingException("Audio extraction failed.")
+
+    return audio_only_path
+
+
+def get_transcription_text(file_path: Path, entity: EntityProxy) -> str:
+    model = settings.whisper_model
+    models_path = Path(settings.whisper_model_root)
+
+    output_path = Path(settings.data_root) / file_path.parts[-1].split(".")[0]
+
+    cmd = [
+        settings.whisper_executable,
+        "-m",
+        models_path / model,
+        "-f",
+        file_path,
+        "-oj",
+        "-of",
+        output_path,
+        "-l",
+        # "auto" sometimes transcribes audio in an unintended language
+        settings.whisper_language,
+    ]
+
+    try:
+        log.info(cmd)
+        subprocess.run(
+            cmd,
+            timeout=int(settings.whisper_timeout),
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+    except subprocess.CalledProcessError as e:
+        raise e
+
+    output_path = output_path.with_suffix(".json")
+    if not output_path.is_file():
+        raise ProcessingException(
+            f"Transcription failed. This file type might be unsupported: {file_path.parts[-1]}."
+        )
+
+    with open(output_path, "r") as f:
+        transcription_dict = json.loads(f.read())
+
+    transcription_intervals = transcription_dict.get("transcription")
+    if transcription_intervals:
+        full_transcription = ""
+        for interval in transcription_intervals:
+            full_transcription += f"[{interval['timestamps']['from']} -> {interval['timestamps']['to']}] {interval['text'].strip()}"
+        return full_transcription
+    else:
+        _delete_temporary_file(output_path)
+        raise ProcessingException(
+            f"Transcription failed, no output in file {output_path}."
+        )
+
+    _delete_temporary_file(output_path)
+
+
+def _delete_temporary_file(file_path: Path):
+    if not file_path.is_file():
+        return
+
+    Path.unlink(file_path)
